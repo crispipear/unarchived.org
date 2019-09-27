8fbc741b199c9034a9bab725ac253d0e51f95b36
Su Li <sul@isoftstone.com>
2019-09-26 18:54:28 -0700

    updates

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -45,7 +45,7 @@ class Menu extends Component {
   render(){
         return(
           <div className={this.props.siteLoc === '/' ? "menu menu_home" : "menu menu_reg"}
-               style={{right: (this.props.view == 2 && !this.props.openedMenu) ? '-51vw': 0}}
+               style={{right: (this.props.view == 2 && !this.props.openedMenu) ? '-51vw' : this.props.view == 2 ? 0 : 'unset'}}
           >
             <div className="logo">
                   {
